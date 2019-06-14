70c9a71b0344e78ec1a827b01f9f4e127d36f113
surmurat <mrtsrnlr@gmail.com>
2019-06-14 07:36:42 -0400

    "PBD Updated"

--- 20190614/U1M2L1PBDMuratSurenler/src/main/java/ThereOrNot.java
+++ 20190614/U1M2L1PBDMuratSurenler/src/main/java/ThereOrNot.java
@@ -1,9 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class ThereOrNot {
     public static void main(String[] args) {
 
+        List<Integer> numbers = new ArrayList<>();
+        for(int i = 0; i < 10; i++ ){
+            numbers.add(ran());
+        }
+        System.out.println("Array List: "+ numbers);
         Scanner scan = new Scanner(System.in);
+        System.out.print("Value to find: ");
+        int num = Integer.parseInt(scan.nextLine());
+        System.out.println("\n");
+
+        if(numbers.contains(num)){
+            System.out.format("%d is in the ArrayList", num);
+        }else
+            System.out.format("%d is not in the ArrayList", num);
+
+    }
+    public static Integer ran(){
+        Random ran = new Random();
+        Integer x = ran.nextInt(50) + 1;
+        return x;
 
 
     }
--- 20190614/U1M2L1PBDMuratSurenler/src/main/java/WhereIsIt.java
+++ 20190614/U1M2L1PBDMuratSurenler/src/main/java/WhereIsIt.java
@@ -1,2 +1,31 @@
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class WhereIsIt {
-}
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        for(int i = 0; i < 10; i++ ){
+            numbers.add(ran());
+        }
+        System.out.println("Array List: "+ numbers);
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Value to find: ");
+        int num = Integer.parseInt(scan.nextLine());
+        System.out.println("\n");
+
+        if(numbers.contains(num)){
+            int x = numbers.lastIndexOf(num);
+            System.out.format("%d is in slot %d", num, x);
+        }else
+            System.out.format("%d is not in the ArrayList", num);
+
+    }
+    public static Integer ran(){
+        Random ran = new Random();
+        Integer x = ran.nextInt(50) + 1;
+        return x;
+    }
+    }
+
